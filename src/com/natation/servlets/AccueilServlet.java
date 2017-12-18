f23222f644ac3014825e9c6219e0369dd9c2c4eb
ElCappuccino <giraud.valentin69@gmail.com>
2017-12-18 08:50:32 +0100

    Update AccueilServlet.java

--- src/com/natation/servlets/AccueilServlet.java
+++ src/com/natation/servlets/AccueilServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class AccueilServlet
@@ -13,9 +14,25 @@ public class AccueilServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public static final String VUE = "/WEB-INF/accueil.jsp";
+	public static final String REDIRECT = "/connect";
 	public static final String NAME = "accueil";
+	public static final String ATTR_SESSION_USERBEAN = "userBean";
+
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+		HttpSession session = request.getSession();
+		/*
+         * Si l'objet utilisateur n'existe pas dans la session en cours, alors
+         * l'utilisateur n'est pas connecté.
+         */
+        if ( session.getAttribute( ATTR_SESSION_USERBEAN ) == null ) {
+            /* Redirection vers la page publique */
+            response.sendRedirect( request.getContextPath() + REDIRECT );
+        } else {
+            /* Affichage de la page restreinte */
+        	this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+        }
+        
+		
 	}
 }
