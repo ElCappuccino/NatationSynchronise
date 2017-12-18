47999259c48b59938da37d758779b4c012a128d2
ElCappuccino <giraud.valentin69@gmail.com>
2017-12-18 08:49:26 +0100

    Update CompetitionServlet.java

--- src/com/natation/servlets/CompetitionServlet.java
+++ src/com/natation/servlets/CompetitionServlet.java
@@ -5,17 +5,30 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class CompetitionServlet
  */
 public class CompetitionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+	public static final String REDIRECT = "/connect";
 	public static final String VUE = "/WEB-INF/competition.jsp";
+	public static final String ATTR_SESSION_USERBEAN = "userBean";
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+		HttpSession session = request.getSession();
+		/*
+         * Si l'objet utilisateur n'existe pas dans la session en cours, alors
+         * l'utilisateur n'est pas connecté.
+         */
+        if ( session.getAttribute( ATTR_SESSION_USERBEAN ) == null ) {
+            /* Redirection vers la page publique */
+            response.sendRedirect( request.getContextPath() + REDIRECT );
+        } else {
+            /* Affichage de la page restreinte */
+        	this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+        }
 	}
 
 }
