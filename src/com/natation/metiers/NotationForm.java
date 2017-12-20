b1e0bc012dd09585c1342ce81b2ce8b93166896b
Valentin <v.giraud@lyon.ort.asso.fr>
2017-12-20 12:14:14 +0100

    Ajout NotationForm

---
+++ src/com/natation/metiers/NotationForm.java
@@ -0,0 +1,27 @@
+package com.natation.metiers;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import com.natation.beans.CompetitionBean;
+import com.natation.dao.CompetitionDAO;
+
+public class NotationForm {
+	private CompetitionDAO competitionDAO;
+	private Map<String, String> erreurs = new HashMap<>();
+	
+	public NotationForm(CompetitionDAO competitionDAO) {
+		this.competitionDAO = competitionDAO;
+	}
+	
+	public ArrayList<CompetitionBean> getCompetitionsByUser(String idUser) {
+		ArrayList<CompetitionBean> list = null;
+		try {
+			list = competitionDAO.getCompetitionByUtilisateur(idUser);
+		} catch (SQLException e) {
+			erreurs.put("getCompetitionsByUser", e.getMessage());
+		}
+		return list;
+	}
+}
